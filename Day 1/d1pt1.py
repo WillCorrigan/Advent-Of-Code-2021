2d98d3d7af9b703e23516feef4826aeb0bf9a065
Will <wcorrigan90@gmail.com>
2021-12-01 11:22:40 +0000

    aoc d1

---
+++ Day 1/d1pt1.py
@@ -0,0 +1,7 @@
+filename = "C:\\Users\\wcorr\\Desktop\\AoC 2021\\Day 1\\data.txt"
+with open(filename) as f:
+    content = f.readlines()
+
+content = [int(x.strip()) for x in content]
+
+print(sum((1 if y > x else 0) for x, y in zip(content, content[1:])))
