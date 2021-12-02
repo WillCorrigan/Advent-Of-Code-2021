2bd4eda3140eff21f6c6e89769318ba65c4fd371
Will <wcorrigan90@gmail.com>
2021-12-02 14:24:16 +0000

    d2p2

--- Day 2/d2pt1.py
+++ Day 2/d2pt1.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 filename = "C:\\Users\\wcorr\\Desktop\\AoC 2021\\Day 2\\data.txt"
@@ -6,8 +5,9 @@ with open(filename) as f:
     content = f.readlines()
 
 content = [[x[0], int(x[1])] for x in (y.strip().split(" ") for y in content)]
+
 summed_content = [[k, v] for k, v in pd.DataFrame(content).groupby(0).sum()[1].items()]
 
 answer = (summed_content[0][1] - summed_content[2][1]) * summed_content[1][1]
 
-print(answer)
+print(summed_content)
--- Day 2/d2pt2.py
+++ Day 2/d2pt2.py
@@ -1,1 +1,20 @@
-# tbc
+filename = "C:\\Users\\wcorr\\Desktop\\AoC 2021\\Day 2\\data.txt"
+with open(filename) as f:
+    content = f.readlines()
+
+content = [[x[0], int(x[1])] for x in (y.strip().split(" ") for y in content)]
+
+aim = 0
+horizontal = 0
+depth = 0
+
+for x in content:
+    if x[0] == 'forward':
+        horizontal += x[1]
+        depth += x[1] * aim
+    if x[0] == 'down':
+        aim += x[1]
+    if x[0] == 'up':
+        aim -= x[1]
+
+print(horizontal * depth)
